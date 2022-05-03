cd8ae2e52a6af339b7b6c1d8e9e326299cf20840
Allef <alleftsoares@gmail.com>
2022-05-02 23:48:07 -0500

    Files

---
+++ polybar/launch.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/sh
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -x polybar >/dev/null; do sleep 1; done
+
+# Launch polybar
+# polybar example &
+polybar left &
+polybar center &
+polybar right  &
+#polybar left &
+#polybar mid &
+#polybar right &
